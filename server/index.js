d988599b06a6fd1443c5295ea324c546b0b786aa
nehhav <nehhav@hotmail.com>
2021-10-25 10:29:09 +0530

    add project model

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express, { application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+
+const zomato = express();
+
+//application middleware
+zomato.use(express.json);
+zomato.use(express.urlencoded({ extended: false }));
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/", (req, res) => res.json({ message: "Setup success" }));
+
+zomato.listen(4000, () => console.log("Server is running 🚀 "));
+
